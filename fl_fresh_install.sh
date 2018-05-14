3959679b585c68e20b159e5b4ac08e7c98636d71
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2018-05-15 00:56:57 +0530

    Update fl_fresh_install.sh

--- fl_fresh_install.sh
+++ fl_fresh_install.sh
@@ -17,7 +17,7 @@ echo "Some php.ini tweaks"
 sleep 2;
 sudo sed -i "s/post_max_size = .*/post_max_size = 2000M/" /etc/php/7.0/fpm/php.ini
 sudo sed -i "s/memory_limit = .*/memory_limit = 3000M/" /etc/php/7.0/fpm/php.ini
-sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 1000M/" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.0/fpm/php.ini
 sudo sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php/7.0/fpm/php.ini
 sudo sed -i "s/; max_input_vars = .*/max_input_vars = 5000/" /etc/php/7.0/fpm/php.ini
 sudo sed -i "s/zlib.output_compression = Off/zlib.output_compression = on/" /etc/php/7.0/fpm/php.ini
